2c00522cd1ae50099085c3893daa1667439b3007
liwei217 <liwei217@wanda.cn>
2016-12-26 19:42:42 +0800

    memcloud-data-dao

---
+++ memcloud-data-mongodb/src/main/java/org/memcloud/data/mongodb/App.java
@@ -0,0 +1,13 @@
+package org.memcloud.data.mongodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
