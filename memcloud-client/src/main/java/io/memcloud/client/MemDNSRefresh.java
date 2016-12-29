c25515b86557a575b9d8b438a8205ba522e1b167
liwei217 <liwei217@wanda.cn>
2016-12-29 18:33:43 +0800

    memclient dns refresh log

--- memcloud-client/src/main/java/io/memcloud/client/MemDNSRefresh.java
+++ memcloud-client/src/main/java/io/memcloud/client/MemDNSRefresh.java
@@ -1,14 +1,27 @@
 package io.memcloud.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.sf.json.JSONObject;
 
 public class MemDNSRefresh implements IMemDNSRefresh {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MemDNSRefresh.class);
 
 	@Override
 	public MemDNSLookup refreshDNS(String appid) {
 		String api = getMemdnsAddr(appid);
+		
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("memdns lookup request: {}", api);
+		}
 		String jsonText = HttpUtil.doGet(api, "UTF-8");
 		//DEMO: {"attachment":{"appId":10001,"version":1,"groupText":"10\u002e10\u002e83\u002e178\u003a14503\u002c10\u002e10\u002e83\u002e177\u003a14504","shardNum":1,"timestamp":1341368772169,"ttlSecond":10},"debug":"","message":"SUCC","status":200}
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("memdns lookup response: {}", jsonText);
+		}
+		
 		JSONObject jsonObj = JSONObject.fromObject(jsonText);
 		if (jsonObj.getInt("status") != 200) {
 			return null;
